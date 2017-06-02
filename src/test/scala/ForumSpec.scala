3f2994fab4f472cad72ecd1319714387a70bd7af
plonacazyrafa <katarzyna.kosek@hotmail.com>
2017-06-02 17:22:48 +0200

    Refactor

--- src/test/scala/ForumSpec.scala
+++ src/test/scala/ForumSpec.scala
@@ -1,4 +1,5 @@
 import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.Inside._
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import akka.http.scaladsl.model.StatusCodes
 import slick.jdbc.H2Profile.api.Database
@@ -7,11 +8,32 @@ class ForumSpec extends {
   val db = Database.forConfig("mydb")
   } with WordSpec with Matchers with ScalatestRouteTest with ServerRoute {
   "The service should" {
-    "return topic example" in {
-      Get("/topic/0") ~> route ~> check {
-        responseAs[Topic] shouldEqual Topic(0, "testAlias", "testEmail", "testContent", "testTopic", "0000")
+    "return non-empty topic with one reply for id=1" in {
+      Get("/topic/1") ~> route ~> check {
+        inside(responseAs[TopicWithReplies]) {
+          case TopicWithReplies(topic, replies) =>
+            inside(topic) { case Topic(id, alias, email, _, _, _) =>
+              id should be(1)
+              //TODO
+            }
+            replies should have length 1
+        }
+      }
+    }
+    "return one topic with two replies for id=2" in {
+      Get("/topic/2") ~> route ~> check {
+        inside(responseAs[TopicWithReplies]) {
+          case TopicWithReplies(topic, replies) =>
+            inside(topic) { case Topic(id, alias, email, _, _, _) =>
+              id should be(2)
+              //TODO
+            }
+            replies should have length 2
+        }
       }
     }
     1
   }
+
+  it
 }
